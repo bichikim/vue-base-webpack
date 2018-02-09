d4eec5611046accf074afd7df1d1d9a9f7170733
bichi <bichi@live.co.kr>
2018-02-09 19:04:59 +0900

    update

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Bichi Kim <bichi@pjfactory.com>
+ * @copyright PJ Factory Co.
+ * @license Private
+ */
+import {vueLoader} from '../lib/webpack/utils'
+export default (config) => {
+
+}
+
+export {utils}
