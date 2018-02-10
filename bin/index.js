e603e2364343cc8d59ba1ea3d433a890f97d2bbc
Bichi Kim <bichi@live.co.kr>
2018-02-10 23:43:57 +0900

    update

--- bin/index.js
+++ bin/index.js
@@ -4,17 +4,19 @@
  * @copyright PJ Factory Co.
  * @license Private
  */
-import webpackBaseConf from '../lib/webpack/webpack.base.conf'
+import webpackProdConf from '../lib/webpack/webpack.prod.conf'
 import webpackDevConf from '../lib/webpack/webpack.dev.conf'
 import webpackMerge from 'webpack-merge'
-import {DEVELOPMENT, PRODUCTION, TEST} from '../lib/webpack/Config'
+import {Config, DEVELOPMENT, PRODUCTION, TEST} from '../lib/webpack/Config'
 export default (config, state, webpackConfig = {}) => {
-  let _webpackConfig = webpackBaseConf(config, state)
+  const conf = new Config(config)
   switch(state){
   case DEVELOPMENT:
-    _webpackConfig = webpackMerge(_webpackConfig, webpackDevConf(config))
+    return webpackMerge(webpackDevConf(conf), webpackConfig)
+  case PRODUCTION:
+    return webpackMerge(webpackProdConf(conf), webpackConfig)
   }
-  return webpackMerge(_webpackConfig, webpackConfig)
+
 }
 
 export {DEVELOPMENT, PRODUCTION, TEST}
